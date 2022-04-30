03713bcc2901d05c3da7af2cd99eec4e280f40bb
David Soh <SotongDJ@users.noreply.github.com>
2022-04-30 18:36:31 +0800

    fiix typo

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def on_message(message):
             beauty_msg = '現在在 <#{}> 有開啟 <#{}> 討論串，歡迎到討論串參與討論'
             with open("record.json",'w') as target_handle:
                 json.dump(record_dict,target_handle,indent=0)
-            sorted_thread_list = sorted([n for n in record_dict.keys()], key=lamda x : record_dict[x])
+            sorted_thread_list = sorted([n for n in record_dict.keys()], key=lambda x : record_dict[x])
             beautify_member_list = [beauty_msg.format(record_dict[k],k) for k in sorted_thread_list]
             if target_id != 0:
                 delete_msg = await target_channel.fetch_message(target_id)
