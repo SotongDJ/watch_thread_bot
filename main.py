e40439977b95f65bb801de11efa350db4cb447d2
DavidSoh[BR11] <mail@stdj.me>
2022-05-01 10:40:55 +0800

    use new record_dict format
    and adjust msg format

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def on_message(message):
             for guild in client.get_all_channels():
                 if isinstance(guild, discord.TextChannel):
                     for thread in guild.threads:
-                        record_dict[thread.id] = thread.parent_id
+                        record_dict[thread.id] = {"parent_id":thread.parent_id,"name":thread.name}
             server_str = readJ("settings.json","server")
             channel_str = readJ("settings.json","channel")
             target_id = readJ("settings.json","anchor")
@@ -49,24 +49,27 @@ async def on_message(message):
             with open("record.json",'w') as target_handle:
                 json.dump(record_dict,target_handle,indent=0)
             sorted_thread_list = sorted([n for n in record_dict.keys()], key=lambda x : record_dict[x])
-            beautify_member_list = list() 
-            beauty_text_msg = '現在在 <#{}> 有開啟 <#{}> 討論串，歡迎到討論串參與討論'
-            beauty_archived_msg = '現在在 <#{}> 有 [封存討論串](https://discord.com/channels/{}/{}) 討論串，歡迎到討論串參與討論'
+            beautify_msg_list = list()
+            beautify_embed_msg_list = list()
+            beauty_msg = '＃{n} 討論串： https://discord.com/channels/{s}/{c}'
+            beauty_embed_none_msg = '現在在 <#{p}> 有 ＃{n} 討論串，歡迎到 [＃{n}](https://discord.com/channels/{s}/{c}) 討論串參與討論'
+            beauty_embed_text_msg = '現在在 <#{p}> 有開啟 ＃{n} 討論串，歡迎到 <#{c}> 討論串參與討論'
             for k in sorted_thread_list:
                 thread_channel = client.get_channel(k)
+                beautify_msg_list.append(beauty_msg.format(n=record_dict[k]["name"],s=server_str,c=k))
                 if isinstance(thread_channel, NoneType):
-                    beautify_member_list.append(beauty_archived_msg.format(record_dict[k],server_str,k))
+                    beautify_embed_msg_list.append(beauty_embed_none_msg.format(p=record_dict[k]["parent_id"],n=record_dict[k]["name"],s=server_str,c=k))
                 else:
-                    beautify_member_list.append(beauty_text_msg.format(record_dict[k],k))
+                    beautify_embed_msg_list.append(beauty_embed_text_msg.format(p=record_dict[k]["parent_id"],n=record_dict[k]["name"],c=k))
             if target_id != 0:
                 delete_msg = await target_channel.fetch_message(target_id)
                 await delete_msg.delete()
             embed_msg = discord.Embed(
                 title="討論串集散地", 
-                description="\n".join(beautify_member_list), 
+                description="\n".join(beautify_embed_msg_list), 
                 color=discord.Color.blue()
             )
-            target_msg = await target_channel.send(embed=embed_msg)
+            target_msg = await target_channel.send(content="\n".join(beautify_msg_list),embed=embed_msg)
             await hi_msg.edit(content=F"完成！請前往<#{channel_str}>查看\n🔗：https://discord.com/channels/{server_str}/{channel_str}/{target_msg.id}")
 
 token = open("token.txt").read().splitlines()[0]
